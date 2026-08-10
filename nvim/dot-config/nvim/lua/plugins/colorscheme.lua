1b3cbe6e1f2194bbc481dbb2a2954bb3ba9a6847
Sergii Tykhomyrov <sergii@tykhomyrov.net>
2026-08-10 22:48:15 +0200

    nvim: install zenbones.nvim

---
+++ nvim/dot-config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,9 @@
+return {
+  "mcchrish/zenbones.nvim",
+  dependencies = { "rktjmp/lush.nvim" },
+  lazy = false,
+  priority = 1000,
+  config = function()
+    vim.cmd.colorscheme("zenwritten")
+  end,
+}
